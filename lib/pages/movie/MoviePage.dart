4b8daecd70edbbee903a5c9155e9e6eb0031738e
manageritcast <manageritcast@126.com>
2018-10-19 21:43:21 +0800

    4-使用Dio发送网络请求

--- lib/pages/movie/MoviePage.dart
+++ lib/pages/movie/MoviePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
-import 'dart:convert';
+import 'package:dio/dio.dart';
 
 class MoviePage extends StatelessWidget {
   @override
@@ -35,16 +34,19 @@ class MoviePageState extends State<MoviePageWidget> {
     super.initState();
 
     // 获取正在热映的数据
-    _getMovieListDataByType('in_theaters');
+    // _getMovieListDataByType('in_theaters');
 
     // 获取即将上映的数据
-    _getMovieListDataByType('coming_soon');
+    // _getMovieListDataByType('coming_soon');
 
     // 获取Top250的数据
-    _getMovieListDataByType('top250');
+    // _getMovieListDataByType('top250');
+
+    _getMovieListData();
   }
 
   // 获取 正在热映 & 即将上映 & Top250 的数据
+  /*
   void _getMovieListDataByType(type) async {
     var httpClient = new HttpClient();
     try {
@@ -76,6 +78,42 @@ class MoviePageState extends State<MoviePageWidget> {
       }
     } catch (exception) {}
   }
+  */
+
+  // 使用dio实现
+  _getMovieListData() async {
+    var dio1 = new Dio(new Options(extra: {"type": 'in_theaters'}));
+    var dio2 = new Dio(new Options(extra: {"type": 'coming_soon'}));
+    var dio3 = new Dio(new Options(extra: {"type": 'top250'}));
+    List<Response> responses = await Future.wait([
+      dio1.get("https://api.douban.com/v2/movie/in_theaters?start=0&count=10"),
+      dio2.get("https://api.douban.com/v2/movie/coming_soon?start=0&count=10"),
+      dio3.get("https://api.douban.com/v2/movie/top250?start=0&count=10")
+    ]);
+
+    // 遍历返回的数据，赋值给对应的数组
+    responses.map((Response response) {
+      print(response.data["subjects"]);
+      switch (response.request.extra["type"]) {
+        case "in_theaters":
+          setState(() {
+            _inTheatersList = response.data["subjects"];
+          });
+          break;
+        case "coming_soon":
+          setState(() {
+            _comingSoonList = response.data["subjects"];
+          });
+          break;
+        case "top250":
+          setState(() {
+            _top250List = response.data["subjects"];
+          });
+          break;
+        default:
+      }
+    }).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,6 +122,11 @@ class MoviePageState extends State<MoviePageWidget> {
     print(_comingSoonList);
     print(_top250List);
     // TODO: implement build
-    return new Scaffold(body: new Center(child: new Text('电影')));
+    return new Scaffold(
+        body: new RaisedButton(
+            child: new Center(child: new Text('发请求666')),
+            onPressed: () {
+              this._getMovieListData();
+            }));
   }
 }
